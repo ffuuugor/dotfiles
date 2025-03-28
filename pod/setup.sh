540c7f552e843fb8d931e4563f59951337324036
Igor Shilov <hello@igorshilov.com>
2025-03-28 11:15:52 +0000

    auto-accept nodejs reinstall

--- pod/setup.sh
+++ pod/setup.sh
@@ -17,7 +17,7 @@ git config --global user.email "hello@igorshilov.com"
 git config --global user.name "Igor Shilov"
 
 # Update Node.js to latest version
-sudo apt-get remove nodejs
+sudo apt-get remove -y nodejs
 sudo dpkg --remove --force-remove-reinstreq libnode-dev
 curl -fsSL https://deb.nodesource.com/setup_23.x | sudo -E bash -
 sudo apt-get install -y nodejs

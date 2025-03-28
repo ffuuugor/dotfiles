ddf28e98cb45cbc7cdc8d93cab9b0c65fff1d443
Igor Shilov <hello@igorshilov.com>
2025-03-28 11:07:16 +0000

    install claude code

--- pod/setup.sh
+++ pod/setup.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Script location: $SCRIPT_DIR"
+#
+# RunPod Environment Setup
+#
 
+# Install sudo if not available
 su -c 'apt-get update && apt-get install -y sudo'
-sudo apt-get install -y less nano htop ncdu nvtop lsof rsync btop jq tmux
 
+sudo apt-get install -y less nano htop ncdu nvtop lsof rsync btop jq tmux npm vim
+
+# Apply .bashrc
 cp /workspace/dotfiles/pod/.bashrc $HOME/.bashrc
 source $HOME/.bashrc
 
+# Configure git user details
 git config --global user.email "hello@igorshilov.com"
 git config --global user.name "Igor Shilov"
+
+# Update Node.js to latest version
+sudo apt-get remove nodejs
+sudo dpkg --remove --force-remove-reinstreq libnode-dev
+curl -fsSL https://deb.nodesource.com/setup_23.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Claude code
+npm install -g @anthropic-ai/claude-code
